8b120a8e5ecf31d979a487ad5c498958c28fb7eb
v-zhgl <38325459+Zheng-Li01@users.noreply.github.com>
2022-05-12 14:27:48 +0800

    Update slngen.cmd

--- slngen.cmd
+++ slngen.cmd
@@ -1,12 +1,10 @@
 SET ROOT= %~dp0
-SET INIT = %ROOT%init.cmd
-SET SLNGEN =  %ROOT%.corext\gen\InPath\slngen.cmd
 
 @REM -- call the init.cmd to restore the packages.
-@call INIT
+@call %ROOT%\init.cmd
 
 @REM -- back to the root of the azure computer after running the init.cmd, whcih wil point to C:\src\Azure-Compute\src
-@cd /d %ROOT%
+@cd /d  %ROOT%
 
 @REM -- call the slngen to generate the solution.
-@call  SLNGEN -vs "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" --launch false
+@call  %ROOT%\slngenCommnad.cmd
